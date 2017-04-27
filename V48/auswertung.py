334bba14947f94c8b29b59c26339e3bfcbd8f8ef
Dag Hering <dag.hering@tu-dortmund.de>
2017-04-27 10:46:23 +0200

    V48 neu

---
+++ V48/auswertung.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import uncertainties.unumpy as unp
+from uncertainties.unumpy import (nominal_values as noms,
+                                  std_devs as stds)
+from scipy import stats
+from scipy.optimize import curve_fit
+import scipy.constants as const
+from uncertainties import ufloat
