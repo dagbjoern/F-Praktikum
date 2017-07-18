ae5773824029541fda3ac060286ed1b018a2bb03
Dag-Björn Hering <dag.hering@tu-dortmund.de>
2017-07-18 19:03:52 +0200

    V27 zwischen save

--- V27/auswertung.py
+++ V27/auswertung.py
@@ -77,6 +77,8 @@ J=1
 g_3P1=landefak(J,S,L)
 print('g_i von 3P1 =',g_3P1)
 
+DlamD_rot=lamDelta(lam_rot,d,n_rot)
+DlamD_blau=lamDelta(lam_blau,d,n_blau)
 print('Despersionsgebiet rot=',lamDelta(lam_rot,d,n_rot))
 print('Despersionsgebiet blau=',lamDelta(lam_blau,d,n_blau))
 print('Auflöungsvermögen rot=',lamDelta(lam_rot,L,n_rot))
@@ -135,6 +137,84 @@ plt.plot(I_lin,hysterese(I_lin,*params_hyst),'c-',label=r'Fit')
 # plt.plot(T1[j:],I1[j:]-gerade(T1[j:],*params_untergund1),'mx',)
 # plt.plot(T1,I1,'kx',label=r'Messpunkte mit Untergrund')
 plt.legend(loc='best')
-plt.xlabel(r'Strom $I/ \si{\ampere} $')
-plt.ylabel(r'Magnetfeld $B/ \si{\milli\tesla}$')
+#plt.xlabel(r'Strom $I/ \si{\ampere} $')
+#plt.ylabel(r'Magnetfeld $B/ \si{\milli\tesla}$')
 plt.savefig('build/plot_hyst.pdf')
+
+
+rot_Ds= 1510-1280  # D entspricht Delta
+print(rot_Ds)
+rot_ds1=1360-1220  # d entspricht delta
+print(rot_ds1)
+rot_ds2=1555-1440
+print(rot_ds2)
+
+rot_ds=unp.uarray(np.mean([rot_ds1,rot_ds2]),np.std([rot_ds1,rot_ds2]))
+print(rot_ds)
+blau_sig_Ds=2725-2515
+print(blau_sig_Ds)
+blau_sig_ds1=2580-2480
+print(blau_sig_ds1)
+blau_sig_ds2=2780-2685
+print(blau_sig_ds2)
+
+blau_sig_ds=unp.uarray(np.mean([blau_sig_ds1,blau_sig_ds2]),np.std([blau_sig_ds1,blau_sig_ds2]))
+print(blau_sig_ds)
+
+blau_pi_Ds=2945-2740
+print(blau_pi_Ds)
+blau_pi_ds1=2790-2710
+print(blau_pi_ds1)
+blau_pi_ds2=2985-2905
+print(blau_pi_ds2)
+blau_pi_ds=unp.uarray(np.mean([blau_pi_ds1,blau_pi_ds2]),np.std([blau_pi_ds1,blau_pi_ds2]))
+print(blau_pi_ds)
+
+
+def d_lam(Ds,ds,DlamD):
+    return (1/2) * (ds/Ds)*DlamD
+
+def d_E_lam(lam,d_lam):
+    return -const.h*(const.c/lam**2)*d_lam
+
+def g_mess(d_E,B):
+    mu_b=const.value("Bohr magneton")
+    return d_E/( mu_b*B)
+
+def hysterese_unp(x,A,x0,C):
+    return A*(unp.arctan((x-x0)/C))
+
+print('A=',A)
+print('I_0=',x0)
+print('C=',C)
+
+I_rot=unp.uarray(11.8,0.5)
+I_blau_sig=unp.uarray(5.1,0.5)
+I_blau_pi=unp.uarray(17.0,0.5)
+
+
+B_rot=hysterese_unp(I_rot,A,x0,C)/1000
+B_blau_sig=hysterese_unp(I_blau_sig,A,x0,C)/1000
+B_blau_pi=hysterese_unp(I_blau_pi,A,x0,C)/1000
+
+print('rot B',B_rot)
+d_lam_rot=d_lam(rot_Ds,rot_ds,DlamD_rot)
+print('d_lam-rot',d_lam_rot)
+d_E_lam_rot=d_E_lam(lam_rot,d_lam_rot)
+print('d_E_lam_rot',d_E_lam_rot/const.e)
+print('g_rot',g_mess(d_E_lam_rot,B_rot))
+
+print('blau B_sig',B_blau_sig)
+d_lam_sig_blau=d_lam(blau_sig_Ds,blau_sig_ds,DlamD_blau)
+print('d_lam_sig_blau',d_lam_sig_blau)
+d_E_lam_blau_sig=d_E_lam(lam_blau,d_lam_sig_blau)
+print('d_E_lam_blau_sig',d_E_lam_blau_sig/const.e)
+print('g_blau_sig',g_mess(d_E_lam_blau_sig,B_blau_sig))
+
+
+print('blau_B_pi',B_blau_pi)
+d_lam_pi_blau=d_lam(blau_pi_Ds,blau_pi_ds,DlamD_blau)
+print('d_lam_pi_blau',d_lam_pi_blau)
+d_E_lam_blau_pi=d_E_lam(lam_blau,d_lam_pi_blau)
+print('d_E_lam_blau_pi',d_E_lam_blau_pi/const.e)
+print('g_blau_pi',g_mess(d_E_lam_blau_pi,B_blau_pi))
